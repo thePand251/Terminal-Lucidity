422fec4167e7df907ed37687847ffa28a5f5bcbd
SimonCoolCat <108771093+SimonCoolCat64@users.noreply.github.com>
2026-04-23 14:32:31 -0700

    dummy is now murderable
    finished hit/hurtboxes, collision, enemy health, etc

--- assets/scripts/bullet.gd
+++ assets/scripts/bullet.gd
@@ -4,6 +4,8 @@ var bulletImpact = preload("res://assets/scenes/areas/bullet_impact.tscn")
 
 var speed : int = 400
 var direction : int
+var damage : int = 1
+
 func _physics_process(delta):
 	move_local_x(direction * speed * delta)
 
@@ -13,12 +15,17 @@ func _on_timer_timeout():
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	print("bullet area entered")
+	bullet_impact()
 	
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	print("bullet body entered")
+	bullet_impact()
 
 func bullet_impact():
-	var bullet_impact_effect_instance = bulletImpact.instagate() as Node2D
+	var bullet_impact_effect_instance = bulletImpact.instantiate() as Node2D
 	bullet_impact_effect_instance.global_position = global_position
 	get_parent().add_child(bullet_impact_effect_instance)
 	queue_free()
+
+func get_damage_amount() -> int:
+	return damage
--- assets/scripts/enemy_test.gd
+++ assets/scripts/enemy_test.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var deathEffect = preload("res://assets/scenes/areas/enemy_death_effect.tscn")
+var health : int = 3
+
 const gravity = 1000
 
 func _ready() -> void:
@@ -15,3 +18,13 @@ func enemy_gravity(delta : float):
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	print("hurtbox area entered")
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health -= node.damage
+		print("Dummy heath:", health)
+		
+		if health <= 0:
+			var enemy_death_effect_instance = deathEffect.instantiate() as Node2D
+			enemy_death_effect_instance.global_position = global_position
+			get_parent().add_child(enemy_death_effect_instance)
+			queue_free()
